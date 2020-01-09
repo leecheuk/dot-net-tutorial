161c2cd9bc113ca58fc0a202e1f38dddcfbd7efd
m <dev.cheuk.lee@gmail.com>
2020-01-08 21:19:31 -0500

    changed format and added movies link

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models {
     public class Movie {
@@ -8,10 +9,13 @@ namespace RazorPagesMovie.Models {
 
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         // show date only
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
