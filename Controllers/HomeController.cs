5e7ea1fb59f8d7fc46e1082393c7e934b7cfa963
m <dev.cheuk.lee@gmail.com>
2020-01-09 22:41:41 -0500

    added movies api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace webAppTutorial.Controllers
 {
     public class ApiController : Controller
     {
 
+        private readonly RazorPagesMovieContext _context;
+
+        public ApiController(RazorPagesMovieContext ctx) {
+            _context = ctx;
+        }
+        
         // GET; /api/
         public string Index()
         {
@@ -18,5 +28,14 @@ namespace webAppTutorial.Controllers
             // add query string
             return HtmlEncoder.Default.Encode($"Hello {name}, welcome to api world! NumTimes is: {numTimes}");
         }
+
+        // GET; /api/movies
+        [Route("api/movies")]
+        public IEnumerable<Movie> GetMovies() {
+            IQueryable<Movie> query = _context.Movie;
+            return query.ToList();
+        }
+
+
     }
 }
